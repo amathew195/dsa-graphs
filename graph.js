1f4681fa0f63b4b9b37bc077ea34898fb89c00a3
Ashley Mathew <amathew195@gmail.com>
2022-12-15 13:48:24 -0600

    completed up to DFS

--- graph.js
+++ graph.js
@@ -16,26 +16,66 @@ class Graph {
   }
 
   /** add Node instance and add it to nodes property on graph. */
-  addVertex(vertex) { }
+  addVertex(vertex) {
+    // let node = new Node(vertex);
+    this.nodes.add(vertex);
+  }
 
   /** add array of new Node instances and adds to them to nodes property. */
-  addVertices(vertexArray) { }
+  addVertices(vertexArray) {
+    for (const vertex of vertexArray) {
+      this.nodes.add(vertex);
+    }
+  }
 
   /** add edge between vertices v1,v2 */
-  addEdge(v1, v2) { }
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   /** remove edge between vertices v1,v2 */
-  removeEdge(v1, v2) { }
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   /** remove vertex from graph:
    *
    * - remove it from nodes property of graph
    * - update any adjacency lists using that vertex
    */
-  removeVertex(vertex) { }
+  removeVertex(vertex) {
+
+    //remove it from nodes property on graph
+    this.nodes.delete(vertex);
+
+    //update any adjacency lists using that vertex
+    for (const neighbor of vertex.adjacent) {
+      neighbor.adjacent.delete(vertex);
+    }
+
+  }
 
   /** traverse graph with DFS and returns array of Node values */
-  depthFirstSearch(start) { }
+  depthFirstSearch(start) {
+    let toVisitStack = [start];
+    let seen = new Set(toVisitStack);
+    const nodesArr = [];
+
+    while (toVisitStack.length > 0) {
+      let currNode = toVisitStack.pop();
+      nodesArr.push(currNode.value);
+
+      for (let neighbor of currNode.adjacent) {
+        if (!seen.has(neighbor)) {
+          toVisitStack.push(neighbor);
+          seen.add(neighbor);
+        }
+      }
+    }
+    return nodesArr;
+  }
 
   /** traverse graph with BDS and returns array of Node values */
   breadthFirstSearch(start) { }
@@ -44,4 +84,4 @@ class Graph {
   distanceOfShortestPath(start, end) { }
 }
 
-module.exports = { Graph, Node }
+module.exports = { Graph, Node };
